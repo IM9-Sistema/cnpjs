e0061127ec634c21244509f26584b49a62c96c1b
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-02-06 10:30:30 -0300

    `Init` - Maven

---
+++ csearch/src/main/java/br/com/im9/jaobernardi/App.java
@@ -0,0 +1,13 @@
+package br.com.im9.jaobernardi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
